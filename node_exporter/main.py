d1519038123f47e8c0e977230b2f403b983d5d92
alexandre <alexandreserras@ua.pt>
2022-04-29 14:15:43 +0100

    translator do node

---
+++ node_exporter/main.py
@@ -0,0 +1,26 @@
+from tkinter.messagebox import NO
+from orwell import Metric, Runner
+
+
+def translate (line: str) -> list:
+    if line.startswith("#"): return None
+    line = line.split(" ")
+    ts=line[-1]
+    value=line[-2]
+    properties=line[:-2]
+    properties="_".join(properties)
+    if len(properties) > 1:
+            properties = "_".join(properties)
+    else:
+        properties=properties[0]
+    array = properties.split("{")
+    if len(array) > 1:
+        properties = { v[0]:v[1].strip("\"}") for v in [ v.split("=") for v in array[1].split(",") ] } if len(array[1]) > 1 else {}
+    else:
+        properties=dict()
+    title=array[0]
+    return Metric(title, value, properties, ts)
+translator = Runner(translate)
+translator.run()
+
+
