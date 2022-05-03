3c3c5bc7873eecd6b7a9291143bd8d76a748ec43
alexandre <alexandreserras@ua.pt>
2022-05-03 17:16:23 +0100

    finnaly ?

--- gnochi/main.py
+++ gnochi/main.py
@@ -1,33 +1,28 @@
-from orwell import Metric, Runner,MetricsFactory
+from orwell import Metric, Runner, MetricsFactory
 import json
+
+
 
 def translate (dic: dict) -> list:
     dic = json.loads(dic)
     metrics_lst=[]
-    print(dic)
+
+    if 'vcpus' in dic.keys():
+      for cpu in range(int(dic['vcpus'][2])):
+        metrics_lst.append(MetricsFactory.create_cpu_time_metric(cpu, "", "0", dic['vcpus'][0], dic['instance']))
+
+    if 'memory' in dic.keys():
+      metrics_lst.append(MetricsFactory.create_memory_total_metric(str(dic['memory'][2]), dic['memory'][0], dic['instance']))
+    
     if "memory.usage" in dic.keys():
       dic["memory_free"]=[ dic["memory.usage"][0],dic["memory.usage"][1],
       dic["memory"][2]- dic["memory.usage"][2] ]
-      dic.pop("memory.usage")
-    properties= dict()
-    properties["instance"]=dic["instance"]
-    dic.pop("instance")
-    for key,value in dic.items():
-        data = Metric(key, value[2], properties,  value[0],properties["instance"])
-        metrics_lst.append(data)
-    print(metrics_lst)
+      metrics_lst.append(MetricsFactory.create_memory_available_metric(dic["memory_free"][2],dic["memory_free"][0],dic['instance']))
+    
+    if "disk.root.size" in dic.keys():
+      metrics_lst.append(MetricsFactory.create_filesystem_size_metric("","","/",int(dic["disk.root.size"][2])*1024**3,dic["disk.root.size"][0],dic['instance']))
     return metrics_lst
-def create_metric (title: str, value: str, properties: dict, ts: str) -> Metric:
-  
-  if title == 'memory': return MetricsFactory.create_memory_total_metric(value, ts)
-  elif title == 'disk.root.size': return MetricsFactory.create_filesystem_size_metric("", "","", value, ts)
-  
- 
-  elif title == 'compute.instance.booting.time': return [ MetricsFactory.create_up_time_metric(value, ts), MetricsFactory.create_boot_time_metric('0', ts) ]
 
-  elif title== "disk.ephemeral.size": return Metric("ephemeral_size", value, properties, ts)
-  return Metric(title, value, properties, ts)
 
 translator = Runner(translate)
 translator.run()
-
