8d01649ad1669bc0547136e3d31d9428d50573b1
alexandre <alexandreserras@ua.pt>
2022-05-04 11:45:04 +0100

    fix dos valores das metricas

--- gnochi/main.py
+++ gnochi/main.py
@@ -13,12 +13,12 @@ def translate (dic: dict) -> list:
         metrics_lst.append(MetricsFactory.create_cpu_time_metric(cpu, "system", "0", dic['vcpus'][0], dic['instance']))
 
     if 'memory' in dic.keys():
-      metrics_lst.append(MetricsFactory.create_memory_total_metric(str(int(dic['memory'][2])), dic['memory'][0], dic['instance']))
+      metrics_lst.append(MetricsFactory.create_memory_total_metric(str(int(dic['memory'][2])*1024*1024), dic['memory'][0], dic['instance']))
     
     if "memory.usage" in dic.keys():
       dic["memory_free"]=[ dic["memory.usage"][0],dic["memory.usage"][1],
       dic["memory"][2]- dic["memory.usage"][2] ]
-      metrics_lst.append(MetricsFactory.create_memory_available_metric(str(int(dic["memory_free"][2])),dic["memory_free"][0],dic['instance']))
+      metrics_lst.append(MetricsFactory.create_memory_available_metric(str(int(dic["memory_free"][2])*1024*1024),dic["memory_free"][0],dic['instance']))
     
     if "disk.root.size" in dic.keys():
       #tmp=int(dic["disk.root.size"][2])*1024**3      metrics_lst.append(MetricsFactory.create_filesystem_size_metric("vda1", "ext4","/",str(int(dic["disk.root.size"][2]*1024**3)),dic["disk.root.size"][0],dic['instance']))
