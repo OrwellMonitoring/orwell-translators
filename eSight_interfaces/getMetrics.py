adeb554c34960d5422c73c507c06465c8d612501
alexandre <alexandreserras@ua.pt>
2022-06-06 11:47:04 +0100

    eSight interfaces code done

---
+++ eSight_interfaces/getMetrics.py
@@ -0,0 +1,14 @@
+import requests
+import warnings
+import json
+import math
+
+
+from kafka import KafkaProducer
+
+
+KAFKA_URL= requests.get("http://10.0.12.82:8008/service_discovery/kafka/").text
+
+with open('example.txt') as json_file:
+    data = json.load(json_file)
+KafkaProducer(bootstrap_servers=[KAFKA_URL]).send('esight_interface', json.dumps(data).encode('ascii'))
