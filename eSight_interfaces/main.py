2f18b29fc712c02549325938a8cfb5452d1d8e65
alexandre <alexandreserras@ua.pt>
2022-06-11 14:19:02 +0100

    bug fix

--- eSight_interfaces/main.py
+++ eSight_interfaces/main.py
@@ -1,16 +1,17 @@
 from orwell import Metric, Runner
 import json
 import time
+import logging
 def translate (line: str) -> list:
     line = json.loads(line)
     res = []
-    print(line)
+    logging.info(line)
     title="esight_interface"
     for key in line.keys():
         interface_name = key
         #Data have all metrics now lets split them
         try:
-            send_data=line[interface_name]["sending_rate"]["data"]["resultData"][0]
+            send_data=line[interface_name]["sending_rate"]["resultData"][0]
             send_interface_user_friendly_name=send_data["neName"]
             send_index_units= send_data["indexUnit"]
             properties={ "interface_name": interface_name, "units":send_index_units, "interface_user_friendly_name":send_interface_user_friendly_name}
@@ -19,7 +20,7 @@ def translate (line: str) -> list:
                     send_index_units="Kbps" 
                     values["indexValue"]=values["indexValue"]*1000
                 res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(int(time.time())),"perf"))
-            receive_data=line[interface_name]["receiving_rate"]["data"]["resultData"][0]
+            receive_data=line[interface_name]["receiving_rate"]["resultData"][0]
             receive_interface_user_friendly_name=receive_data["neName"]
             receive_index_units= receive_data["indexUnit"]
             properties={"interface_name": interface_name, "units":receive_index_units, "interface_user_friendly_name":receive_interface_user_friendly_name}
@@ -29,9 +30,9 @@ def translate (line: str) -> list:
                     values["indexValue"]=values["indexValue"]*1000
                 res.append(Metric(title+"_receive_interface", str(values["indexValue"]), properties, str(int(time.time())),"perf"))
         except:
-            print("NO DATA")
+            logging.info("ERRO AO CRIAR MÉTRICAS")
             continue
     return res
-
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 translator = Runner(translate)
 translator.run()
