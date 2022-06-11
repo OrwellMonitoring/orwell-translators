1390d519f5c61cb3b04a5b1c104707985740e65d
goncalo-leal <goncalolealsilva@ua.pt>
2022-06-11 17:21:59 +0100

    refactor

--- eSight_interfaces/main.py
+++ eSight_interfaces/main.py
@@ -6,47 +6,75 @@ import logging
 # we added this flag because we were having an error with Prometheus
 # change this flag if you want to keep the original timestamps
 change_timestamp = True
+
 def translate (line: str) -> list:
     line = json.loads(line)
+    logging.info("Received a new message")
+
     res = []
-    logging.info(line)
     title="esight_interface"
     for key in line.keys():
         interface_name = key
-        #Data have all metrics now lets split them
-        try:
-            send_data=line[interface_name]["sending_rate"]["resultData"][0]
-            send_interface_user_friendly_name=send_data["neName"]
-            send_index_units= send_data["indexUnit"]
-            properties={ "interface_name": interface_name, "units":send_index_units, "interface_user_friendly_name":send_interface_user_friendly_name}
+
+        if "sending_rate" in line[interface_name].keys() \
+            and "resultData" in line[interface_name]["sending_rate"].keys() \
+            and len(line[interface_name]["sending_rate"]["resultData"]) > 0:
+
+            logging.info(f"Translating sending rate metrics for {interface_name}")
+
+            send_data = line[interface_name]["sending_rate"]["resultData"][0]
+
+            send_interface_user_friendly_name = send_data["neName"]
+            send_index_units = send_data["indexUnit"]
+
+            properties = {"interface_name": interface_name, "units": send_index_units, "interface_user_friendly_name": send_interface_user_friendly_name}
             for values in send_data["indexValues"]:
+
                 if send_index_units == "Mbps":
-                    send_index_units="Kbps" 
-                    values["indexValue"]=values["indexValue"]*1000
+                    # convert value from Mbps to Kbps
+                    send_index_units = "Kbps" 
+                    values["indexValue"] = values["indexValue"] * 1000
+
                 if change_timestamp:
                     time.sleep(2)
-                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(int(time.time())),"perf"))
+                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(int(time.time())), "perf"))
                 else:
-                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"perf"))
-            receive_data=line[interface_name]["receiving_rate"]["resultData"][0]
-            receive_interface_user_friendly_name=receive_data["neName"]
-            receive_index_units= receive_data["indexUnit"]
-            properties={"interface_name": interface_name, "units":receive_index_units, "interface_user_friendly_name":receive_interface_user_friendly_name}
+                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]), "perf"))
+        else:
+            logging.info(f"{interface_name} has sent a mesage with no sending rate metrics")
+        
+        if "receiving_rate" in line[interface_name].keys() \
+            and "resultData" in line[interface_name]["receiving_rate"].keys() \
+            and len(line[interface_name]["receiving_rate"]["resultData"]) > 0:
+
+            logging.info(f"Translating receiving rate metrics for {interface_name}")
+
+            receive_data = line[interface_name]["receiving_rate"]["resultData"][0]
+
+            receive_interface_user_friendly_name = receive_data["neName"]
+            receive_index_units = receive_data["indexUnit"]
+
+            properties={"interface_name": interface_name, "units": receive_index_units, "interface_user_friendly_name": receive_interface_user_friendly_name}
             for values in receive_data["indexValues"]:
                 if receive_index_units == "Mbps":
+                    # convert value from Mbps to Kbps
                     receive_index_units="Kbps" 
-                    values["indexValue"]=values["indexValue"]*1000
+                    values["indexValue"]=values["indexValue"] * 1000
+
                 if change_timestamp:
                     time.sleep(2)
                     res.append(Metric(title+"_receive_interface", str(values["indexValue"]), properties, str(int(time.time())),"perf"))
                 else:
                     res.append(Metric(title+"_receive_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"perf"))
+        else:
+            logging.info(f"{interface_name} has sent a mesage with no receiving rate metrics")
 
-        except:
-            logging.info("ERRO AO CRIAR MÉTRICAS")
-            continue
-    logging.info("Métricas a serem enviadas")
+
+    logging.info(f"Sending Metrics from {interface_name}")
+    logging.info(res)
     return res
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+logging.basicConfig(format='[%(levelname)s] - %(asctime)s -> %(message)s', level=logging.INFO, datefmt='%d-%m-%Y %H:%M:%S')
+
 translator = Runner(translate)
 translator.run()
