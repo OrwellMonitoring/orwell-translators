0c3d4eb3014f7d249559b05817c16a6cd1614cc7
alexandre <alexandreserras@ua.pt>
2022-06-12 16:40:10 +0100

    slots fix

--- eSight_routers/main.py
+++ eSight_routers/main.py
@@ -14,35 +14,31 @@ def translate (line: str) -> list:
     for key in line.keys():
         interface_name = key
         #Data have all metrics now lets split them
-        try:
-            for x in line[interface_name]["cpu_usage"]["resultData"]:
-            
-                cpu_usage=x
-                cpu_name=cpu_usage["indexName"]
-                cpu_units= cpu_usage["indexUnit"]
-                cpu_display_key= cpu_usage["displayValueKey"]
-                properties={ "interface_name": interface_name, "units":cpu_units, "cpu_name":cpu_name,"cpu_display_key":cpu_display_key}
-                for values in cpu_usage["indexValues"]:
-                    if change_timestamp:
-                        time.sleep(2)
-                        res.append(Metric(title+"_cpu_usage", str(values["indexValue"]), properties, str(int(time.time())),"esight_slots"))
-                    else:
-                        res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"esight_slots"))
-            for x in line[interface_name]["mem_usage"]["resultData"]:
-                mem_usage=x
-                mem_name=mem_usage["indexName"]
-                mem_units= mem_usage["indexUnit"]
-                mem_display_key= mem_usage["displayValueKey"]
-                properties={ "interface_name": interface_name, "units":mem_units, "cpu_name":mem_name,"cpu_display_key":mem_display_key}
-                for values in mem_usage["indexValues"]:
-                    if change_timestamp:
-                        time.sleep(2)
-                        res.append(Metric(title+"_mem_usage", str(values["indexValue"]), properties, str(int(time.time())),"esight_slots"))
-                    else:
-                        res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"esight_slots"))
-        except:
-            logging.info("NO DATA")
-            continue
+        for x in line[interface_name]["cpu_usage"]["resultData"]:
+        
+            cpu_usage=x
+            cpu_name=cpu_usage["neName"]
+            cpu_units= cpu_usage["indexUnit"]
+            cpu_display_key= cpu_usage["displayValueKey"]
+            properties={ "interface_name": interface_name, "units":cpu_units, "cpu_name":cpu_name,"cpu_display_key":cpu_display_key}
+            for values in cpu_usage["indexValues"]:
+                if change_timestamp:
+                    time.sleep(2)
+                    res.append(Metric(title+"_cpu_usage", str(values["indexValue"]), properties, str(int(time.time())),"esight_slots"))
+                else:
+                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"esight_slots"))
+        for x in line[interface_name]["mem_usage"]["resultData"]:
+            mem_usage=x
+            mem_name=mem_usage["neName"]
+            mem_units= mem_usage["indexUnit"]
+            mem_display_key= mem_usage["displayValueKey"]
+            properties={ "interface_name": interface_name, "units":mem_units, "cpu_name":mem_name,"cpu_display_key":mem_display_key}
+            for values in mem_usage["indexValues"]:
+                if change_timestamp:
+                    time.sleep(2)
+                    res.append(Metric(title+"_mem_usage", str(values["indexValue"]), properties, str(int(time.time())),"esight_slots"))
+                else:
+                    res.append(Metric(title+"_send_interface", str(values["indexValue"]), properties, str(values["timestamp"]),"esight_slots"))
     return res
 
 logging.basicConfig(format='[%(levelname)s] - %(asctime)s -> %(message)s', level=logging.INFO, datefmt='%d-%m-%Y %H:%M:%S')
